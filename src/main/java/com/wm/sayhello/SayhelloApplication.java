0b185c6d6fb7d6266841cf80320d8457405be1dc
wm <15871491953@163.com>
2019-09-04 18:29:27 +0800

    Initial commit

---
+++ src/main/java/com/wm/sayhello/SayhelloApplication.java
@@ -0,0 +1,13 @@
+package com.wm.sayhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayhelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayhelloApplication.class, args);
+	}
+
+}
